35432394cdd4217c0ddfd503ae46d28937bf948e
Décio Francis <francisdasilva@gmail.com>
2023-03-07 21:51:53 -0300

    criando funcao editar repositorio

--- src/servicos/requisicoes/repositorios.js
+++ src/servicos/requisicoes/repositorios.js
@@ -10,3 +10,19 @@ export async function pegarRepositoriosDoUsuario(id) {
         return [];
     }
 }
+
+export async function salvarRepositoriosDoUsuario(postId, nome, data, id) {
+    try {
+        await api.put(`/repos?${id}`, {
+            name: nome,
+            data: data,
+            postId: postId,
+            id: id
+        });
+        return 'sucesso'
+    }
+    catch(error){
+        console.log(error)
+        return 'erro'
+    }
+}
